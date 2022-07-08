b17843f60f6de9082d4a8ca2353ed683ce72116b
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-08 14:02:23 -0700

    Added ConnectionString

--- IceCubey_Server/Program.cs
+++ IceCubey_Server/Program.cs
@@ -1,11 +1,16 @@
+using IceCubey_DataAccess.Data;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
