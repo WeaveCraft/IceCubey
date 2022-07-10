5941aa24ec1c252cf599867e2a776c705392b4e0
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-10 15:39:02 -0700

    Added CategoryDTO

---
+++ IceCubey_Business/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,13 @@
+﻿using IceCubey_Models;
+
+namespace IceCubey_Business.Repository.IRepository
+{
+    public interface ICategoryRepository
+    {
+        public CategoryDTO Create(CategoryDTO objDTO);
+        public CategoryDTO Update(CategoryDTO objDTO);
+        public int Delete(int id);
+        public CategoryDTO Get(int id);
+        public IEnumerable<CategoryDTO> GetAll();
+    }
+}
--- IceCubey_DataAccess/Category.cs
+++ IceCubey_DataAccess/Category.cs
@@ -1,7 +1,10 @@
-﻿namespace IceCubey_DataAccess
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IceCubey_DataAccess
 {
     public class Category
     {
+        [Key]
         public int Id { get; set; }
         public string? Name { get; set; }
         public DateTime CreatedDate { get; set; }
