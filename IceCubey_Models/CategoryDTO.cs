03216b8b96291f037dac7e4ce20490b0ac486245
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-11 11:50:29 -0700

    Added validation for Category Page

--- IceCubey_Models/CategoryDTO.cs
+++ IceCubey_Models/CategoryDTO.cs
@@ -1,8 +1,11 @@
-﻿namespace IceCubey_Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IceCubey_Models
 {
     public class CategoryDTO
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Please Enter a Name")]
         public string? Name { get; set; }
     }
 }
