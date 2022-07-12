16c45b90ce3564468b9d595982aeac431c779b93
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-12 13:46:30 -0700

    Edit/Update on Categories now works. And Everything is Async.

--- IceCubey_Business/Repository/CategoryRepository.cs
+++ IceCubey_Business/Repository/CategoryRepository.cs
@@ -3,6 +3,7 @@ using IceCubey_Business.Repository.IRepository;
 using IceCubey_DataAccess;
 using IceCubey_DataAccess.Data;
 using IceCubey_Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace IceCubey_Business.Repository
 {
@@ -17,50 +18,50 @@ namespace IceCubey_Business.Repository
             _mapper = mapper;
         }
 
-        public CategoryDTO Create(CategoryDTO objDTO)
+        public async Task<CategoryDTO> Create(CategoryDTO objDTO)
         {
             var obj = _mapper.Map<CategoryDTO, Category>(objDTO);
             obj.CreatedDate = DateTime.Now;
-            var addedObj = _db.Categories.Add(obj); //Create variable to see what has been added
-            _db.SaveChanges();
+            var addedObj = _db.Categories.Add(obj);
+            await _db.SaveChangesAsync();
 
             return _mapper.Map<Category, CategoryDTO>(addedObj.Entity);
         }
 
-        public int Delete(int id)
+        public async Task<int> Delete(int id)
         {
-            var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            var obj = await _db.Categories.FirstOrDefaultAsync(u => u.Id == id);
             if (obj != null)
             {
                 _db.Categories.Remove(obj);
-                return _db.SaveChanges();
+                return await _db.SaveChangesAsync();
             }
-            return 0;  //Return 0 for "delete was no successfull"
+            return 0;
         }
 
-        public CategoryDTO Get(int id)
+        public async Task<CategoryDTO> Get(int id)
         {
-            var obj = _db.Categories.FirstOrDefault(u => u.Id == id);
+            var obj = await _db.Categories.FirstOrDefaultAsync(u => u.Id == id);
             if (obj != null)
             {
-                _mapper.Map<Category, CategoryDTO>(obj);
+                return _mapper.Map<Category, CategoryDTO>(obj);
             }
             return new CategoryDTO();
         }
 
-        public IEnumerable<CategoryDTO> GetAll()
+        public async Task<IEnumerable<CategoryDTO>> GetAll()
         {
             return _mapper.Map<IEnumerable<Category>, IEnumerable<CategoryDTO>>(_db.Categories);
         }
 
-        public CategoryDTO Update(CategoryDTO objDTO)
+        public async Task<CategoryDTO> Update(CategoryDTO objDTO)
         {
-            var objFromDb = _db.Categories.FirstOrDefault(u => u.Id == objDTO.Id);
-            if(objFromDb != null)
+            var objFromDb = await _db.Categories.FirstOrDefaultAsync(u => u.Id == objDTO.Id);
+            if (objFromDb != null)
             {
                 objFromDb.Name = objDTO.Name;
                 _db.Categories.Update(objFromDb);
-                _db.SaveChanges();
+                await _db.SaveChangesAsync();
                 return _mapper.Map<Category, CategoryDTO>(objFromDb);
             }
             return objDTO;
