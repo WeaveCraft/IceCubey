8013092ba36adac9af27ca0f4654945f854b47c4
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-12 16:12:37 -0700

    Added Expense Page.

--- IceCubey_Server/Program.cs
+++ IceCubey_Server/Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<ICategoryRepository, CategoryRepository>(); //When we request category repo in blazor server it will give us an obj of categoryrepo & its implementation.
 builder.Services.AddScoped<IIncomeRepository, IncomeRepository>(); 
+builder.Services.AddScoped<IExpenseRepository, ExpenseRepository>(); 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 var app = builder.Build();
