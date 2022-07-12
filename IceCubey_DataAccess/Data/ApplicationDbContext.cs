63eb2b675c1ac9c0f1952f8078ebea1d8cba06ae
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-12 14:39:43 -0700

    Added Incomes Table to DB with Income Model

--- IceCubey_DataAccess/Data/ApplicationDbContext.cs
+++ IceCubey_DataAccess/Data/ApplicationDbContext.cs
@@ -10,5 +10,6 @@ namespace IceCubey_DataAccess.Data
         }
 
         public DbSet<Category> Categories { get; set; }
+        public DbSet<Income> Incomes { get; set; }
     }
 }
--- IceCubey_DataAccess/Migrations/20220712213800_AddedIncomeToDb.Designer.cs
+++ IceCubey_DataAccess/Migrations/20220712213800_AddedIncomeToDb.Designer.cs
@@ -4,6 +4,7 @@ using IceCubey_DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace IceCubey_DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220712213800_AddedIncomeToDb")]
+    partial class AddedIncomeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
