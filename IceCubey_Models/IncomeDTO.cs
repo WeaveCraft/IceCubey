34c06434580a0a8f4f1eb961d7e30ff98767669b
Viktor Hurtig <Viktor.hurtig@outlook.com>
2022-07-13 12:11:05 -0700

    Can now create Expenses & Incomes with a Category.

--- IceCubey_Models/IncomeDTO.cs
+++ IceCubey_Models/IncomeDTO.cs
@@ -10,6 +10,7 @@ namespace IceCubey_Models
         public string? Description { get; set; }
         public bool IsCommon { get; set; } //Common income such as pay
         public bool IsUncommon { get; set; } //Income that simply is uncommon
+        [Required]
         public DateTime Date { get; set; }
         public string? ImageUrl { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Please select a category")]
